3ca6f3e6bff4add73afc582c912da6a4150a092f
Nel1yMinecraft <917752541@qq.com>
2024-02-04 21:03:43 +0800

    更新 GuiMainMenu.java

--- src/cn/langya/sun/ui/screen/GuiMainMenu.java
+++ src/cn/langya/sun/ui/screen/GuiMainMenu.java
@@ -1,24 +1,31 @@
 package cn.langya.sun.ui.screen;
 
+import cn.langya.sun.Sun;
+import cn.langya.sun.ui.font.FontManager;
 import net.minecraft.client.gui.*;
 
 
 public class GuiMainMenu extends GuiScreen {
 
+    int defaultHeight = height / 4 + 30;
+    int defaultWidth = width / 2 - 60;
+    int buttonWidth = 120;
+    int buttonHeight = 20;
+
     public void initGui() {
-        int defaultHeight = height / 4 + 30;
-        int defaultWidth = width / 2 - 60;
-        int buttonWidth = 120;
-        int buttonHeight = 20;
         buttonList.add(new GuiButton(0, defaultWidth, defaultHeight, buttonWidth, buttonHeight, "Single Player"));
         buttonList.add(new GuiButton(1, defaultWidth, defaultHeight + 25, buttonWidth, buttonHeight, "Multi Player"));
         buttonList.add(new GuiButton(2, defaultWidth, defaultHeight + 50, buttonWidth, buttonHeight, "Game Options"));
-        buttonList.add(new GuiButton(3, defaultWidth, defaultHeight + 75, buttonWidth, buttonHeight, "Quit Game"));
+        buttonList.add(new GuiButton(3, defaultWidth, defaultHeight + 75, buttonWidth, buttonHeight, "Language"));
+        buttonList.add(new GuiButton(4, defaultWidth, defaultHeight + 100, buttonWidth, buttonHeight, "Quit Game"));
         super.initGui();
     }
 
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         drawBackground(0);
+
+        FontManager.T50.drawString(Sun.name,width / 2 - 50, height / 4, -1);
+        FontManager.T20.drawString("Copyright Mojang AB. Do not distribute!",5f, height - 15, -1);
 
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
@@ -36,6 +43,9 @@ public class GuiMainMenu extends GuiScreen {
                 mc.displayGuiScreen(new GuiOptions(this, mc.gameSettings));
                 break;
             case 3:
+                mc.displayGuiScreen(new GuiLanguage(this, mc.gameSettings, mc.getLanguageManager()));
+                break;
+            case 4:
                 mc.shutdown();
                 break;
         }
